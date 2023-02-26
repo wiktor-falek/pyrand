c138fc35bd417bc0ee624bc02e23d89c1d306385
wiktor-falek <falekwiktor@gmail.com>
2023-02-26 22:43:39 +0100

    Nitpicky modifications, start implementing sample().

--- src/pyrand.ts
+++ src/pyrand.ts
@@ -9,7 +9,7 @@ function random(): number {
 }
 
 /**
- * Returns a random integer from a range between *start* and *stop* (inclusive).
+ * Returns a random integer from a range between *start* and *stop* (including the endpoint).
  * @param {number} start
  * @param {number} stop
  * @returns {number}
@@ -36,7 +36,7 @@ function randint(start: number, stop: number): number {
  */
 function shuffle(array: Array<any>): void {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
@@ -119,6 +119,100 @@ function choices(
   }
 
   return result;
+}
+
+/*
+Chooses k unique random elements from a population sequence or set.
+
+Returns a new list containing elements from the population while
+leaving the original population unchanged.  The resulting list is
+in selection order so that all sub-slices will also be valid random
+samples.  This allows raffle winners (the sample) to be partitioned
+into grand prize and second place winners (the subslices).
+
+Members of the population need not be hashable or unique.  If the
+population contains repeats, then each occurrence is a possible
+selection in the sample.
+
+Repeated elements can be specified one at a time or with the optional
+counts parameter.  For example:
+
+    sample(['red', 'blue'], counts=[4, 2], k=5)
+
+is equivalent to:
+
+    sample(['red', 'red', 'red', 'red', 'blue', 'blue'], k=5)
+
+To choose a sample from a range of integers, use range() for the
+population argument.  This is especially fast and space efficient
+for sampling from a large population:
+
+    sample(range(10000000), 60)
+*/
+function sample(
+  population: Array<any>,
+  k: number,
+  counts?: Array<number> | null
+): Array<any> {
+  const n = population.length;
+  if (k <= 0 || k > n) {
+    throw new Error("Sample larger than population or is negative");
+  }
+  if (counts != null) {
+    const cumCounts = accumulate(counts);
+    if (cumCounts.length != n) {
+      throw new Error("The number of counts does not match the population");
+    }
+    const total = cumCounts.pop();
+    if (total == null || !Number.isInteger(total)) {
+      throw new Error("Couts must be integers");
+    }
+
+    if (total <= 0) {
+      throw new Error("Total of counts must be greater than zero");
+    }
+
+    const range = Array(3)
+      // @ts-ignore
+      .fill()
+      .map((_, i) => i);
+    const selections = sample(range, k);
+
+    const result = [];
+    for (const s of selections) {
+      result.push(population[bisectRight(cumCounts, s)]);
+    }
+    return result;
+  }
+  /*
+  randbelow = self._randbelow
+        if not 0 <= k <= n:
+            raise ValueError("Sample larger than population or is negative")
+        result = [None] * k
+        setsize = 21        # size of a small set minus size of an empty list
+        if k > 5:
+            setsize += 4 ** _ceil(_log(k * 3, 4))  # table size for big sets
+        if n <= setsize:
+            # An n-length list is smaller than a k-length set.
+            # Invariant:  non-selected at pool[0 : n-i]
+            pool = list(population)
+            for i in range(k):
+                j = randbelow(n - i)
+                result[i] = pool[j]
+                pool[j] = pool[n - i - 1]  # move non-selected item into vacancy
+        else:
+            selected = set()
+            selected_add = selected.add
+            for i in range(k):
+                j = randbelow(n)
+                while j in selected:
+                    j = randbelow(n)
+                selected_add(j)
+                result[i] = population[j]
+        return result
+  */
+
+  return [];
 }
 
 module.exports = {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+/**
+ * Return the index where to insert item x in list a, assuming a is sorted.
+ * 
+ * The return value i is such that all e in a[:i] have e <= x, and all e in
+ * a[i:] have e > x.  So if x already appears in the list, a.insert(i, x) will
+ * insert just after the rightmost x already there.
+ * 
+ * Optional args lo (default 0) and hi (default len(a)) bound the
+ * slice of a to be searched.
+*/
 export function bisectRight(
   a: Array<number>,
   x: number,
